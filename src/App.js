269b5a52ae175c353aacda03534de3e05c1b0940
santanu banerjee <santanub65@gmail.com>
2021-11-18 09:19:00 +0530

    mini project using React hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App=()=> {
+  let time=new Date().toLocaleTimeString();
+  const[currentTime,setCurrentTime]=useState(time);
+  const updateTime=()=>{
+    let time=new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
+  setInterval(updateTime,1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{currentTime}</h1>
     </div>
   );
 }
